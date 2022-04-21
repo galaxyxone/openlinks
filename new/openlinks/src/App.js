fa582c25025e55c42f31c419d888141e0a438752
Logan Lentz <logan.lentz1@gmail.com>
2022-04-21 00:07:17 -0500

    finally

--- new/openlinks/src/App.js
+++ new/openlinks/src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { Route } from "react-router-dom";
+import Home from "./Home";
+import Profile from "./Profile";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+    <>
+    <Route path="/" exact component={Home} />
+    <Route path="profile" component={Profile} />
+    </>
+    );
+  }
 }
+
 
 export default App;
--- new/openlinks/src/index.js
+++ new/openlinks/src/index.js
@@ -2,7 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import {BrowserRouter as Router} from "react-router-dom";
 import reportWebVitals from './reportWebVitals';
+
+ReactDOM.render(
+  <Router>
+    <Route component={App} />
+  </Router>
+)
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
