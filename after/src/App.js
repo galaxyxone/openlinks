ceceadcb5690c20100bda01f8e07ba3f71d1b202
Logan Lentz <logan.lentz1@gmail.com>
2022-05-02 21:34:58 -0500

    react refactoring

---
+++ after/src/App.js
@@ -0,0 +1,35 @@
+import React, { Component } from "react";
+import { Route } from "react-router-dom";
+import Home from "./Home";
+import Profile from "./Profile";
+import Nav from "./Nav";
+import Auth from "./Auth/Auth";
+import Callback from "./Callback";
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.auth = new Auth(this.props.history);
+  }
+  render() {
+    return (
+      <>
+        <Nav auth={this.auth} />
+        <div className="body">
+          <Route
+            path="/"
+            exact
+            render={props => <Home auth={this.auth} {...props} />}
+          />
+          <Route
+            path="/callback"
+            render={props => <Callback auth={this.auth} {...props} />}
+          />
+          <Route path="/profile" component={Profile} />
+        </div>
+      </>
+    );
+  }
+}
+
+export default App;
---
+++ after/src/Profile.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class Profile extends Component {
+  render() {
+    return <h1>Profile</h1>;
+  }
+}
+
+export default Profile;
