530315ceb9e3e20a8418b5d2f9fbdc0af86549b4
Logan Lentz <logan.lentz1@gmail.com>
2022-05-02 21:56:43 -0500

    React Refactor
    I added the wrong project before, this is the right folder

---
+++ after/server.js
@@ -0,0 +1,68 @@
+const express = require("express");
+require("dotenv").config();
+const jwt = require("express-jwt"); // Validate JWT and set req.user
+const jwksRsa = require("jwks-rsa"); // Retrieve RSA keys from a JSON Web Key set (JWKS) endpoint
+const checkScope = require("express-jwt-authz"); // Validate JWT scopes
+
+const checkJwt = jwt({
+  // Dynamically provide a signing key based on the kid in the header
+  // and the signing keys provided by the JWKS endpoint.
+  secret: jwksRsa.expressJwtSecret({
+    cache: true, // cache the signing key
+    rateLimit: true,
+    jwksRequestsPerMinute: 5, // prevent attackers from requesting more than 5 per minute
+    jwksUri: `https://${
+      process.env.REACT_APP_AUTH0_DOMAIN
+    }/.well-known/jwks.json`
+  }),
+
+  // Validate the audience and the issuer.
+  audience: process.env.REACT_APP_AUTH0_AUDIENCE,
+  issuer: `https://${process.env.REACT_APP_AUTH0_DOMAIN}/`,
+
+  // This must match the algorithm selected in the Auth0 dashboard under your app's advanced settings under the OAuth tab
+  algorithms: ["RS256"]
+});
+
+const app = express();
+
+app.get("/public", function(req, res) {
+  res.json({
+    message: "Hello from a public API!"
+  });
+});
+
+app.get("/private", checkJwt, function(req, res) {
+  res.json({
+    message: "Hello from a private API!"
+  });
+});
+
+app.get("/course", checkJwt, checkScope(["read:courses"]), function(req, res) {
+  res.json({
+    courses: [
+      { id: 1, title: "Building Apps with React and Redux" },
+      { id: 2, title: "Creating Reusable React Components" }
+    ]
+  });
+});
+
+function checkRole(role) {
+  return function(req, res, next) {
+    const assignedRoles = req.user["http://localhost:3000/roles"];
+    if (Array.isArray(assignedRoles) && assignedRoles.includes(role)) {
+      return next();
+    } else {
+      return res.status(401).send("Insufficient role");
+    }
+  };
+}
+
+app.get("/admin", checkJwt, checkRole("admin"), function(req, res) {
+  res.json({
+    message: "Hello from an admin API!"
+  });
+});
+
+app.listen(3001);
+console.log("API server listening on " + process.env.REACT_APP_AUTH0_AUDIENCE);
---
+++ after/src/Public.js
@@ -0,0 +1,23 @@
+import React, { Component } from "react";
+
+class Public extends Component {
+  state = {
+    message: ""
+  };
+
+  componentDidMount() {
+    fetch("/public")
+      .then(response => {
+        if (response.ok) return response.json();
+        throw new Error("Network response was not ok.");
+      })
+      .then(response => this.setState({ message: response.message }))
+      .catch(error => this.setState({ message: error.message }));
+  }
+
+  render() {
+    return <p>{this.state.message}</p>;
+  }
+}
+
+export default Public;
