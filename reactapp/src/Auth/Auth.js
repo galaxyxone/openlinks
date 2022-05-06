3a9a415f0a0d2ab3d0ee825349631d1ba47d7416
Logan Lentz <logan.lentz1@gmail.com>
2022-05-05 23:42:23 -0500

    scopes removed

--- reactapp/src/Auth/Auth.js
+++ reactapp/src/Auth/Auth.js
@@ -4,14 +4,14 @@ export default class Auth {
   constructor(history) {
     this.history = history;
     this.userProfile = null;
-    this.requestedScopes = "openid profile email read:courses";
     this.auth0 = new auth0.WebAuth({
       domain: process.env.REACT_APP_AUTH0_DOMAIN,
       clientID: process.env.REACT_APP_AUTH0_CLIENT_ID,
       redirectUri: process.env.REACT_APP_AUTH0_CALLBACK_URL,
       audience: process.env.REACT_APP_AUTH0_AUDIENCE,
       responseType: "token id_token",
-      scope: this.requestedScopes
+      
+      
     });
   }
 
@@ -43,12 +43,11 @@ export default class Auth {
     // use it to set scopes in the session for the user. Otherwise
     // use the scopes as requested. If no scopes were requested,
     // set it to nothing
-    const scopes = authResult.scope || this.requestedScopes || "";
+   
 
     localStorage.setItem("access_token", authResult.accessToken);
     localStorage.setItem("id_token", authResult.idToken);
     localStorage.setItem("expires_at", expiresAt);
-    localStorage.setItem("scopes", JSON.stringify(scopes));
   };
 
   isAuthenticated() {
@@ -60,7 +59,6 @@ export default class Auth {
     localStorage.removeItem("access_token");
     localStorage.removeItem("id_token");
     localStorage.removeItem("expires_at");
-    localStorage.removeItem("scopes");
     this.userProfile = null;
     this.auth0.logout({
       clientID: process.env.REACT_APP_AUTH0_CLIENT_ID,
@@ -84,10 +82,5 @@ export default class Auth {
     });
   };
 
-  userHasScopes(scopes) {
-    const grantedScopes = (
-      JSON.parse(localStorage.getItem("scopes")) || ""
-    ).split(" ");
-    return scopes.every(scope => grantedScopes.includes(scope));
-  }
+ 
 }
