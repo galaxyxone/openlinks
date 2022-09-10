0d08870cff0aaa3fcb534721cdaf9657102bb54e
Logan Lentz <30084404+noryev@users.noreply.github.com>
2022-09-09 22:55:50 -0500

    rebase for UI
    This is in relation to the UI updates on the homepage

--- src/contexts/stepper-form.context.js
+++
@@ -1,8 +0,0 @@
-import { createContext } from "react";
-
-const StepperFormContext = createContext({});
-
-export const StepperFormContextConsumer = StepperFormContext.Consumer;
-export const StepperFormContextProvider = StepperFormContext.Provider;
-
-export default StepperFormContext;
