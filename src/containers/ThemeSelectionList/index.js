ea386f43791d7df92ad0e9eefcfd212289cd4ecc
Yasir Nadeem <m-yasir.dev@protonmail.com>
2022-11-23 14:15:43 +0500

    refactor(theme-selection-list): simplify Theme Selector Field

--- src/containers/ThemeSelectionList/index.js
+++ src/containers/ThemeSelectionList/index.js
@@ -4,30 +4,15 @@ import { useEffect, useState } from "react";
 import { getThemePreviews } from "@api";
 // user lib
 import ThemeSelector from "@components/ThemeSelector";
-import { useFormContext } from "react-hook-form";
+import { Controller, useFormContext } from "react-hook-form";
 
 const DEFAULT_THEME = "light";
+const FORM_FIELD_NAME = "settings.theme";
 
 function ThemeSelectionList() {
   const [previews, setPreviews] = useState([]); // Theme previews -> From API
-  const [selectedTheme, setSelectedTheme] = useState(() => DEFAULT_THEME);
 
-  const { register, setValue } = useFormContext();
-
-  const formFieldName = "settings.theme";
-
-  // add 'theme' field in our settings form
-  useEffect(() => {
-    register(formFieldName, {
-      value: selectedTheme,
-    });
-  }, [register, selectedTheme]);
-
-  // handle theme selection
-  const handleThemeSelection = (themeName) => {
-    setValue(formFieldName, themeName);
-    setSelectedTheme(themeName);
-  };
+  const { control } = useFormContext();
 
   // get theme previews from API
   useEffect(() => {
@@ -39,16 +24,23 @@ function ThemeSelectionList() {
 
   // render Theme list
   return (
-    <Box display="flex" flexDirection="row" flexWrap="wrap">
-      {previews.map((preview) => (
-        <ThemeSelector
-          key={preview.name}
-          {...preview}
-          onSelect={handleThemeSelection}
-          selected={selectedTheme === preview?.name}
-        />
-      ))}
-    </Box>
+    <Controller
+      control={control}
+      name={FORM_FIELD_NAME}
+      defaultValue={DEFAULT_THEME}
+      render={({ field }) => (
+        <Box display="flex" flexDirection="row" flexWrap="wrap">
+          {previews.map((preview) => (
+            <ThemeSelector
+              key={preview.name}
+              {...preview}
+              onSelect={field.onChange}
+              selected={field.value === preview?.name}
+            />
+          ))}
+        </Box>
+      )}
+    />
   );
 }
 
