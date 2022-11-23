b43be5a026356781b41b20faf156d22638a44443
Yasir Nadeem <m-yasir.dev@protonmail.com>
2022-11-23 21:52:01 +0500

    refactor: move form provider outside form stepper
    This change was made so form values can be shared to form stepper's sibling components

--- src/pages/ExportLinksPage/index.js
+++ src/pages/ExportLinksPage/index.js
@@ -6,36 +6,43 @@ import FormStepper from "@containers/FormStepper";
 import LinkForm from "@containers/LinkForm";
 import LastExported from "@containers/LastExported";
 import SettingsForm from "@containers/SettingsForm";
-import { LinksPageContainer, PageLeftContent, PageRightContent } from "./styles";
+import {
+  LinksPageContainer,
+  PageLeftContent,
+  PageRightContent,
+} from "./styles";
+import { FormProvider, useForm } from "react-hook-form";
 
 export default function ExportLinksPage() {
   const { metadata } = useAuth0Metadata();
+  const formMethods = useForm({ mode: "onBlur" });
   return (
     <ThemePreviewsContextProvider>
-      <LinksPageContainer>
-        <PageLeftContent>
-          <FormStepper
-            steps={[
-              {
-                label: "Fill in Links",
-                content: <LinkForm />,
-                formKey: "links",
-              },
-              {
-                label: "Setup your page",
-                content: <SettingsForm />,
-                formKey: "settings",
-              },
-            ]}
-          />
-          {/* Only show last exported page when user is authenticated and user's data is fetched through management API successfully */}
-          {metadata?.cid && metadata?.filename && (
-            <LastExported cid={metadata.cid} filename={metadata.filename} />
-          )}
-        </PageLeftContent>
-        <PageRightContent>
-        </PageRightContent>
-      </LinksPageContainer>
+      <FormProvider {...formMethods}>
+          <LinksPageContainer>
+            <PageLeftContent>
+              <FormStepper
+                steps={[
+                  {
+                    label: "Fill in Links",
+                    content: <LinkForm />,
+                    formKey: "links",
+                  },
+                  {
+                    label: "Setup your page",
+                    content: <SettingsForm />,
+                    formKey: "settings",
+                  },
+                ]}
+              />
+              {/* Only show last exported page when user is authenticated and user's data is fetched through management API successfully */}
+              {metadata?.cid && metadata?.filename && (
+                <LastExported cid={metadata.cid} filename={metadata.filename} />
+              )}
+            </PageLeftContent>
+            <PageRightContent></PageRightContent>
+          </LinksPageContainer>
+      </FormProvider>
     </ThemePreviewsContextProvider>
   );
 }
