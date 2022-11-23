d15176576778806ef8358c632fbe0d057ad9e6fe
Yasir Nadeem <m-yasir.dev@protonmail.com>
2022-11-23 18:59:07 +0500

    style(export-links-page): divide page into left and right content

--- src/pages/ExportLinksPage/index.js
+++ src/pages/ExportLinksPage/index.js
@@ -6,31 +6,35 @@ import FormStepper from "@containers/FormStepper";
 import LinkForm from "@containers/LinkForm";
 import LastExported from "@containers/LastExported";
 import SettingsForm from "@containers/SettingsForm";
-import { LinksPageContainer } from "./styles";
+import { LinksPageContainer, PageLeftContent, PageRightContent } from "./styles";
 
 export default function ExportLinksPage() {
   const { metadata } = useAuth0Metadata();
   return (
     <ThemePreviewsContextProvider>
       <LinksPageContainer>
-        <FormStepper
-          steps={[
-            {
-              label: "Fill in Links",
-              content: <LinkForm />,
-              formKey: "links",
-            },
-            {
-              label: "Setup your page",
-              content: <SettingsForm />,
-              formKey: "settings",
-            },
-          ]}
-        />
-        {/* Only show last exported page when user is authenticated and user's data is fetched through management API successfully */}
-        {metadata?.cid && metadata?.filename && (
-          <LastExported cid={metadata.cid} filename={metadata.filename} />
-        )}
+        <PageLeftContent>
+          <FormStepper
+            steps={[
+              {
+                label: "Fill in Links",
+                content: <LinkForm />,
+                formKey: "links",
+              },
+              {
+                label: "Setup your page",
+                content: <SettingsForm />,
+                formKey: "settings",
+              },
+            ]}
+          />
+          {/* Only show last exported page when user is authenticated and user's data is fetched through management API successfully */}
+          {metadata?.cid && metadata?.filename && (
+            <LastExported cid={metadata.cid} filename={metadata.filename} />
+          )}
+        </PageLeftContent>
+        <PageRightContent>
+        </PageRightContent>
       </LinksPageContainer>
     </ThemePreviewsContextProvider>
   );
--- src/pages/ExportLinksPage/styles.js
+++ src/pages/ExportLinksPage/styles.js
@@ -1,5 +1,17 @@
 import styled from "styled-components";
 
 export const LinksPageContainer = styled.div`
+  display: flex;
   margin: 16px 16px;
+`;
+
+export const PageLeftContent = styled.section`
+  display: flex;
+  flex-direction: column;
+  width: 50%;
+`;
+
+export const PageRightContent = styled.section`
+  display: flex;
+  width: 50%;
 `;
