5d8c408205372a6fd8430867b6f6500142d42ecd
Yasir Nadeem <m-yasir.dev@protonmail.com>
2022-11-25 18:38:42 +0500

    feat: implement preview components

--- src/containers/LivePreview/styles.js
+++ src/containers/LivePreview/styles.js
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-import frame from '../../assets/preview-device.svg';
+import frame from "../../assets/preview-device.svg";
 
 export const DeviceFrame = styled.div`
   display: block;
@@ -7,10 +7,11 @@ export const DeviceFrame = styled.div`
   left: 50%;
   margin: 0 auto;
   padding: 16px;
+  font-family: "Urbanist", "Times New Roman", Times, serif !important;
   position: absolute;
-  top: 20%;
+  top: 10%;
+  scale: 0.85;
   transform-origin: top left;
-  scale: 0.75;
   width: 352px;
   &::after {
     background-image: url(${frame});
@@ -18,12 +19,67 @@ export const DeviceFrame = styled.div`
     background-repeat: no-repeat;
     background-size: contain;
     content: "";
-    inset: -24px;
+    inset: 0;
     pointer-events: none;
     position: absolute;
-    -webkit-background-size: contain;
-    -moz-background-size: contain;
-    -ms-background-size: contain;
-    -o-background-size: contain;
   }
 `;
+
+export const PreviewLinkButton = styled.a`
+  align-items: center;
+  border-radius: 5px;
+  color: ${({ theme }) => theme?.buttons?.textColor ?? "inherit"};
+  display: flex;
+  font-size: 1rem;
+  font-weight: 500;
+  justify-content: center;
+  margin: 8px 16px;
+  min-height: 50px;
+  text-decoration: none;
+  width: 250px;
+  background-color: ${({ theme }) => theme?.buttons?.bgColor ?? "inherit"};
+  &:hover {
+    cursor: pointer;
+    background-color: ${({ theme }) =>
+      theme?.buttons?.bgColorHover ?? "inherit"};
+  }
+  &:active {
+    background-color: ${({ theme }) =>
+      theme?.buttons?.bgColorActive ?? "inherit"};
+  }
+`;
+
+export const PreviewLinksAvatar = styled.img`
+  background-color: ${({ theme }) => theme?.buttons?.bgColor ?? "inherit"};
+  border-radius: 50%;
+  border: 2px solid ${({ theme }) => theme?.buttons?.bgColor ?? "inherit"};
+  height: 128px;
+  width: 128px;
+  &:hover {
+    background-color: ${({ theme }) =>
+      theme?.buttons?.bgColorHover ?? "inherit"};
+  }
+  &:active {
+    background-color: ${({ theme }) =>
+      theme?.buttons?.bgColorActive ?? "inherit"};
+  }
+`;
+
+export const PreviewLinksContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  overflow-y: scroll; 
+`;
+
+export const PreviewScreen = styled.section`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  color: ${({ theme }) => theme?.buttons?.textColor ?? "inherit"};
+  background-color: ${({ theme }) => theme.container?.bgColor ?? "inherit"};
+  height: 100%;
+  padding: 24px;
+`;
+
+export const PreviewProfileTitle = styled.h1``;
