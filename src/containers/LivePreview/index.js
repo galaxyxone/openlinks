6c7987dd72d57f8d30437dd6d36c47796941d92d
Yasir Nadeem <m-yasir.dev@protonmail.com>
2022-11-25 18:39:00 +0500

    feat: implement live preview

--- src/containers/LivePreview/index.js
+++ src/containers/LivePreview/index.js
@@ -1,15 +1,26 @@
 // node modules
 import { Typography } from "@mui/material";
+import { ThemeProvider } from "styled-components";
 // user lib
+import { useAuth0Metadata } from "@contexts/auth0-metadata.context";
 import useFormValues from "@hooks/useFormValues";
 import useThemeConfig from "@hooks/useThemeConfig";
 // styles
-import { DeviceFrame } from "./styles";
+import {
+  DeviceFrame,
+  PreviewLinksContainer,
+  PreviewLinkButton,
+  PreviewLinksAvatar,
+  PreviewScreen,
+  PreviewProfileTitle,
+} from "./styles";
 
 function LivePreview() {
   const {
     settings: { theme, ...settings },
+    links,
   } = useFormValues();
+  const { metadata } = useAuth0Metadata();
 
   const themeConfig = useThemeConfig(theme);
 
@@ -19,8 +30,28 @@ function LivePreview() {
 
   return (
     <DeviceFrame>
-      {settings?.profileTitle != null && <p>{settings.profileTitle}</p>}
-      <p>{JSON.stringify(themeConfig, null, 2)}</p>
+      <ThemeProvider theme={themeConfig}>
+        <PreviewScreen>
+          <PreviewLinksAvatar src={metadata?.profilePicture} />
+          {settings?.profileTitle && (
+            <PreviewProfileTitle style={themeConfig?.title?.styles}>
+              {settings.profileTitle}
+            </PreviewProfileTitle>
+          )}
+          <PreviewLinksContainer>
+            {links.map((link, idx) => (
+              <PreviewLinkButton
+                key={`${link.title}-${idx}`}
+                href={link.url}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                {link?.title}
+              </PreviewLinkButton>
+            ))}
+          </PreviewLinksContainer>
+        </PreviewScreen>
+      </ThemeProvider>
     </DeviceFrame>
   );
 }
